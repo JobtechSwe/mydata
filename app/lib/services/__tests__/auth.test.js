e27ab7a5ea294a91df2614d81c0ae011f27653d9
Johan Öbrink <johan.obrink@gmail.com>
2019-05-22 08:57:24 +0200

    Fixed the AsyncStorage problem
    ...probably meaning I broke it for everybody else :(

--- app/lib/services/__tests__/auth.test.js
+++ app/lib/services/__tests__/auth.test.js
@@ -87,7 +87,7 @@ describe('auth', () => {
       const jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDAwMC9qd2tzL2NsaWVudF9rZXkifQ.eyJ0eXBlIjoiQVVUSEVOVElDQVRJT05fUkVRVUVTVCIsIm5hbWUiOiJNeSBDViIsImRlc2NyaXB0aW9uIjoiQW4gYXBwIGZvciB5b3VyIENWIG9ubGluZSIsImV2ZW50cyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDAwMC9ldmVudHMiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjQwMDAiLCJhdWQiOiJteWRhdGE6Ly9hdXRoIiwiZXhwIjoxNTU2OTQ2NzcwODM5LCJqdGkiOiI4MjI4YTQ1Yy03MzZlLTQ4M2YtOTI3YS1iODFmMDEwODE3NDAifQ.RrJ1IOToWADw3dx9OZPA1ILnPD1tt195UcLn5ADpaMqKK5qzvz9I_6NqXHfKlozEeRABNAOR5TSVXJv5_CN1PBAxAiucDD8Gez7fOXrxS8vFek5Au6nsqpzTD61pmMmw_wvWPFLhvt_sk46k49oIdnmltqsLm-FKzb7VBmdyd6R_B_Vd9ByTXYr8-2JOQz269zXLoYeV5txTObKlQd_LrGCx9dz3_1N1duMYCcNtknT2mBVbDHWVIKblP6uOyzfDT5LBG9hcCcwJtwSMfwZD6sBp6UpvqLYImCcaat5kBPtyDTdZJEoxcSdIoSWLziAgis1TMeP5mspV3X0Z-JXycA'
       const res = await verifyAndParseAuthRequest(jwt)
       expect(res).toEqual({
-        aud: 'mydata://auth',
+        aud: 'mydata://account',
         description: 'An app for your CV online',
         events: 'http://localhost:4000/events',
         exp: 1556946770839,
@@ -102,7 +102,7 @@ describe('auth', () => {
   describe('#initRegistration', () => {
     it('posts to correct url with correct content type', async () => {
       const authRequest = {
-        aud: 'mydata://auth',
+        aud: 'mydata://account',
         description: 'An app for your CV online',
         events: 'http://localhost:4000/events',
         exp: 1556946770839,
@@ -119,7 +119,7 @@ describe('auth', () => {
 
     it.skip('sends signed jwt', async () => {
       const authRequest = {
-        aud: 'mydata://auth',
+        aud: 'mydata://account',
         description: 'An app for your CV online',
         events: 'http://localhost:4000/events',
         exp: 1556946770839,
