f7119deb8f2150c086cf3013eb7960702c5d4d0c
Tom Ekander <ekander.tom@gmail.com>
2019-06-03 14:02:17 +0200

    chore: beta releases on android and ios

--- app/android/app/src/main/java/com/egendata/MainApplication.java
+++ app/android/app/src/main/java/com/egendata/MainApplication.java
@@ -31,7 +31,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-            new AsyncStoragePackage(),
         new AsyncStoragePackage(),
         new RNSecureRandomPackage(),
         new SvgPackage(),
