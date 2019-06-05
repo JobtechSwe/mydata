183ae3d51cf5afb3851006e1a2d4083f5b10a705
Adam Näslund <adam.naslund@iteam.se>
2019-06-05 12:39:10 +0200

    app: First test of CONNECTION

--- app/lib/services/__tests__/tokens.test.js
+++ app/lib/services/__tests__/tokens.test.js
@@ -31,6 +31,51 @@ describe('tokens', () => {
     expect(token).toEqual(expect.any(String))
   })
 
+  it('CONNECTION', async () => {
+    const connectionRequest = {
+      aud: 'mydata://account',
+      exp: 12331354544,
+      iat: 12331354541,
+      iss: 'http://mycv.work',
+      type: 'CONNECTION_REQUEST',
+      sid: 'e6fb637d-200e-4895-b02b-ab9b3449b181',
+      displayName: 'MyCV - The sexy CV site!',
+      description: 'A service for generating CVs',
+      iconURI: 'http://mycv.work/icon.png',
+    }
+    const token = await tokens.createConnection(connectionRequest, 'e7c525c4-73a0-4838-9559-bc3c9eb76173')
+    expect(token).toEqual(expect.any(String))
+  })
+
+  it('CONNECTION with permissions', async () => {
+  const connectionRequest = {
+    aud: 'mydata://account',
+    exp: 12331354544,
+    iat: 12331354541,
+    iss: 'http://mycv.work',
+    type: 'CONNECTION_REQUEST',
+    permissions: [{
+      domain: 'http://mycv.work',
+      area: 'animal_litter_pictures',
+      id: '4b2aeb88-0837-4bf8-abf3-6ee460598adb',
+      type: 'READ',
+      lawfulBasis: 'CONSENT',
+      jwk: {
+        kty: 'RSA',
+        e: 'AQAB',
+        kid: 'http://mycv.work/jwks/enc_9awsfdu9fjd9jfjjjfffffffffFFF',
+        n: 'qhirpDtQ3u84WY-vh9KrY05FccEwqbynuHgmdBT6q4tHG9iWX1yfw4GEher1KcJiRvMFUGSo3hnIwzi-VJbLrrBZ3As1gUO0SjVEnrJkETEhpFW9f94_rJGelLVvubtPZRzbI-rUOdbNUj6wgZHnWzX9E6dBmzCQ8keHvU9OGWc',
+      },
+    }],
+    sid: 'e6fb637d-200e-4895-b02b-ab9b3449b181',
+    displayName: 'MyCV - The sexy CV site!',
+    description: 'A service for generating CVs',
+    iconURI: 'http://mycv.work/icon.png',
+  }
+  const token = await tokens.createConnection(connectionRequest, 'e7c525c4-73a0-4838-9559-bc3c9eb76173')
+  expect(token).toEqual(expect.any(String))
+})
+
   it('LOGIN', async () => {
     const sessionId = 'yasdiuasdghuiasdads'
     const token = await tokens.createLogin( { serviceId: 'http://mycv.work', connectionId: v4() }, sessionId)
