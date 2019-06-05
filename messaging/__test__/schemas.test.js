6304c5d599eb5b1ce8e6d98303f1ce08a9db586e
Adam Näslund <adam.naslund@iteam.se>
2019-06-05 18:09:17 +0200

    Amended proposition

--- messaging/__test__/schemas.test.js
+++ messaging/__test__/schemas.test.js
@@ -24,7 +24,7 @@ describe('schemas', () => {
         displayName: 'My CV',
         description: 'This is a good CV site',
         iconURI: 'https://cv.work/icon.png',
-        permissions: [
+        permissionRequests: [
           {
             id: '91910133-4024-4641-a7c7-91fb6e11588e',
             domain: 'http://cv.work',
@@ -69,37 +69,31 @@ describe('schemas', () => {
           ...jwtDefaults,
           aud: 'https://smoothoperator',
           type: 'CONNECTION_RESPONSE',
-          content: [
-            {
-              domain: 'https://mycv.work',
-              area: 'education',
-              data: {
-                recipients: [
-                  {
-                    encrypted_key: 'laLxI0j-nLH-_BgLOXMozKxmy9gffy2gTdvqzfTihJBuuzxg0V7yk1WClnQePFvG2K-pvSlWc9BRIazDrn50RcRai__3TDON395H3c62tIouJJ4XaRvYHFjZTZ2GXfz8YAImcc91Tfk0WXC2F5Xbb71ClQ1DDH151tlpH77f2ff7xiSxh9oSewYrcGTSLUeeCt36r1Kt3OSj7EyBQXoZlN7IxbyhMAfgIe7Mv1rOTOI5I8NQqeXXW8VlzNmoxaGMny3YnGir5Wf6Qt2nBq4qDaPdnaAuuGUGEecelIO1wx1BpyIfgvfjOhMBs9M8XL223Fg47xlGsMXdfuY-4jaqVw',
-                    header: {
-                      alg: 'RSA1_5',
-                      kid: 'https://mycv.work/jwks/dfh54g5f4d4',
-                      jwk: jwk()
-                    }
-                  },
-                  {
-                    encrypted_key: 'laLxI0j-nLH-_BgLOXMozKxmy9gffy2gTdvqzfTihJBuuzxg0V7yk1WClnQePFvG2K-pvSlWc9BRIazDrn50RcRai__3TDON395H3c62tIouJJ4XaRvYHFjZTZ2GXfz8YAImcc91Tfk0WXC2F5Xbb71ClQ1DDH151tlpH77f2ff7xiSxh9oSewYrcGTSLUeeCt36r1Kt3OSj7EyBQXoZlN7IxbyhMAfgIe7Mv1rOTOI5I8NQqeXXW8VlzNmoxaGMny3YnGir5Wf6Qt2nBq4qDaPdnaAuuGUGEecelIO1wx1BpyIfgvfjOhMBs9M8XL223Fg47xlGsMXdfuY-4jaqVw',
-                    header: {
-                      alg: 'RSA1_5',
-                      kid: 'mydata://jwks/ulgulgulg',
-                      jwk: jwk()
-                    }
-                  }
-                ],
-                protected: 'dsggff',
-                iv: 'bbd5sTkYwhAIqfHsx8DayA',
-                ciphertext: '0fys_TY_na7f8dwSfXLiYdHaA2DxUjD67ieF7fcVbIR62JhJvGZ4_FNVSiGc_raa0HnLQ6s1P2sv3Xzl1p1l_o5wR_RsSzrS8Z-wnI3Jvo0mkpEEnlDmZvDu_k8OWzJv7eZVEqiWKdyVzFhPpiyQU28GLOpRc2VbVbK4dQKPdNTjPPEmRqcaGeTWZVyeSUvf5k59yJZxRuSvWFf6KrNtmRdZ8R4mDOjHSrM_s8uwIFcqt4r5GX8TKaI0zT5CbL5Qlw3sRc7u_hg0yKVOiRytEAEs3vZkcfLkP6nbXdC_PkMdNS-ohP78T2O6_7uInMGhFeX4ctHG7VelHGiT93JfWDEQi5_V9UN1rhXNrYu-0fVMkZAKX3VWi7lzA6BP430m',
-                tag: 'kvKuFBXHe5mQr4lqgobAUg'
-              }
+          payload: {
+            ...jwtDefaults,
+            type: 'CONNECTION',
+            aud: 'https://mycv.work',
+            sid: 'sdkfhdkskdfd',
+            sub: 'baa949aa-fbb5-4aad-8351-d6ef219dd07b',
+            permissions: {
+              approved: [
+                {
+                  id: 'd8f53525-18a8-4819-a7af-847ed456420f',
+                  domain: 'https://mycv.work',
+                  area: 'edumacation',
+                  type: 'READ'
+                }
+              ],
+              denied: [
+                {
+                  id: '647d1fc5-2a5b-405f-bbd9-1b84d58ce5dc',
+                  domain: 'https://mycv.work',
+                  area: 'diary',
+                  type: 'READ'
+                }
+              ]
             }
-          ],
-          payload: 'asadasdas'
+          }
         }
         await expect(schemas.CONNECTION_RESPONSE.validate(payload))
           .resolves.not.toThrow()
