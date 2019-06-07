f3352983dd1c9b1ca4af2dd3585f94a1c0283554
Johan Öbrink <johan.obrink@gmail.com>
2019-06-07 13:32:16 +0200

    Added tests for new permissions

--- client/__tests__/connection.test.js
+++ client/__tests__/connection.test.js
@@ -3,9 +3,10 @@ const { createMemoryStore } = require('../lib/memoryStore')
 const { generateKeyPair } = require('./_helpers')
 const { JWT } = require('@panva/jose')
 const { createConnectionRequest } = require('./../lib/connection')
+const { schemas } = require('@egendata/messaging')
 
 describe('auth', () => {
-  let clientKeys, config, client
+  let clientKeys, config, payload
   beforeAll(async () => {
     clientKeys = await generateKeyPair()
     config = {
@@ -20,49 +21,68 @@ describe('auth', () => {
       keyValueStore: createMemoryStore(),
       keyOptions: { modulusLength: 1024 }
     }
-    client = createClient(config)
+    payload = {
+      type: 'CONNECTION_INIT',
+      aud: 'http://localhost:51545',
+      iss: 'mydata://account',
+      sid: 'd1f99125-4537-40f1-b15c-fd5e0f067c61',
+      iat: 1558945645,
+      exp: 1558949245
+    }
   })
 
   describe('#createConnectionRequest', () => {
-    it('creates a valid jwt', async () => {
-      const payload = {
-        type: 'CONNECTION_INIT',
-        aud: 'http://localhost:51545',
-        iss: 'mydata://account',
-        sid: 'd1f99125-4537-40f1-b15c-fd5e0f067c61',
-        iat: 1558945645,
-        exp: 1558949245
-      }
+    describe('without defaultPermissions', () => {
+      let client
+      beforeEach(() => {
+        client = createClient(config)
+      })
+      it('creates a valid jwt', async () => {
+        const connReq = await createConnectionRequest(client, payload)
 
-      const connReq = await createConnectionRequest(client, payload)
+        const result = JWT.decode(connReq)
+        expect(result).not.toBe(null)
+      })
+      it('creates a valid message', async () => {
+        const token = await createConnectionRequest(client, payload)
 
-      const result = JWT.decode(connReq)
-      expect(result).not.toBe(null)
+        const message = JWT.decode(token)
+        await expect(schemas.CONNECTION_REQUEST.validate(message))
+          .resolves.not.toThrow()
+      })
     })
-
-    it('Adds permissions if default permissions are configured', async () => {
-      const clientWithDefaultPermissions = createClient({
-        ...config,
-        defaultPermissions: [
+    describe('with defaultPermissions', () => {
+      let client
+      beforeEach(() => {
+        const defaultPermissions = [
           { area: 'education', types: ['READ'], purpose: 'Because i wanna' },
           { area: 'experience', types: ['WRITE'], description: 'Many things about stuff' }
         ]
+        client = createClient({
+          ...config,
+          defaultPermissions
+        })
       })
+      it('creates a valid jwt', async () => {
+        const connReq = await createConnectionRequest(client, payload)
 
-      const connInit = {
-        type: 'CONNECTION_INIT',
-        aud: 'http://localhost:51545',
-        iss: 'mydata://account',
-        sid: 'd1f99125-4537-40f1-b15c-fd5e0f067c61',
-        iat: 1558945645,
-        exp: 1558949245
-      }
+        const result = JWT.decode(connReq)
+        expect(result).not.toBe(null)
+      })
+      it('creates a valid message', async () => {
+        const token = await createConnectionRequest(client, payload)
 
-      const connReq = await createConnectionRequest(clientWithDefaultPermissions, connInit)
+        const message = JWT.decode(token)
+        await expect(schemas.CONNECTION_REQUEST.validate(message))
+          .resolves.not.toThrow()
+      })
+      it('Adds permissions if default permissions are configured', async () => {
+        const connReq = await createConnectionRequest(client, payload)
 
-      const result = JWT.decode(connReq)
+        const result = JWT.decode(connReq)
 
-      expect(result.permissions).toEqual(expect.any(Array))
+        expect(result.permissions).toEqual(expect.any(Array))
+      })
     })
   })
 })
