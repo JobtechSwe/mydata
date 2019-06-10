f020f42e72e4588b514d7d10d1cd7392337e1d0a
Adam Näslund <adam.naslund@iteam.se>
2019-06-10 17:59:47 +0200

    chore: don't console.log :S

--- e2e/jest/permissions.test.js
+++ e2e/jest/permissions.test.js
@@ -46,4 +46,41 @@ describe('Permissions', () => {
 
     client.server.close(done)
   })
+
+  it.skip('correctly sends CONNECTION_REPONSE with default READ permissions', async (done) => {
+    const serviceConfig = {
+      defaultPermissions: [{
+        area: 'favorite_cats',
+        types: ['READ'],
+        purpose: 'To recommend you cats that you\'ll like'
+      }]
+    }
+    let client = await createClientWithServer(serviceConfig)
+    await client.connect()
+
+    // Client library provides auth url and session id
+    const { url, id } = await client.initializeAuthentication()
+
+    // Scan auth and do ping-pong
+    const { connectionRequest } = await phone.handleAuthCode({ code: url })
+
+    /*
+    // Check that the service has stored the key
+    const permissionKey = await client.keyProvider.getKey(connectionRequest.permissions[0].jwk.kid)
+    await JWK.validate(permissionKey.publicKey)
+    await JWK_PRIVATE.validate(permissionKey.privateKey)
+     */
+
+    const userPermissionResult = {
+      approved: connectionRequest.permission
+    }
+    // Approve it!
+    await phone.approveConnection(connectionRequest, 42)
+
+    // After state, expect connection in client for the session id
+    const connectionEntryInClient = await client.keyValueStore.load(`authentication|>${id}`)
+    expect(connectionEntryInClient).toEqual(expect.any(String))
+
+    client.server.close(done)
+  })
 })
