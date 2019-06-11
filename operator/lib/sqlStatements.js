832c62689cd9c94f964277d85e3c66f84b2da4ed
Einar Persson <me@einarpersson.com>
2019-06-11 16:22:33 +0200

    Fix previous commit

--- operator/lib/sqlStatements.js
+++ operator/lib/sqlStatements.js
@@ -43,8 +43,8 @@ function permissionInsert ({
                   description,
                   lawful_basis,
                   read_key,
-                  approvedAt
-                ) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
+                  approved_at
+                ) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
     [
       id,
       connectionId,
